7516a3c4daeb6d64fb4350d7be01ee63c9479e93
Ibrahim <010azaz2@gmail.com>
2024-01-25 18:57:14 +0200

    first half of the screen

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -10,6 +10,50 @@ class HomePage extends StatefulWidget {
 class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Alex"),
+        leading: const Icon(Icons.menu),
+        centerTitle: true,
+      ),
+      body: Column(
+        children: [
+          // using pics
+          Stack(
+            children: [
+              Center(
+                child: Container(
+                  height: 120,
+                  width: 120,
+                  margin: const EdgeInsets.only(top: 4),
+                  decoration: BoxDecoration(
+                    color: Colors.green.shade100,
+                    shape: BoxShape.circle,
+                  ),
+                ),
+              ),
+              Container(
+                height: 123,
+                decoration: const BoxDecoration(
+                    shape: BoxShape.circle,
+                    image: DecorationImage(
+                      image: AssetImage('assets/images/virtualAssistant.png'),
+                    )),
+              )
+            ],
+          ),
+          const SizedBox(
+            height: 40,
+          ),
+          const Text(
+            "heya how can i help!",
+            style: TextStyle(fontSize: 23, fontWeight: FontWeight.w400),
+          ),
+          const SizedBox(
+            height: 20,
+          ),
+        ],
+      ),
+    );
   }
 }
